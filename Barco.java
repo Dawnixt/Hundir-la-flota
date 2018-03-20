a8b9d9d5e833e945ac57e16ac85c8dd6d5be34e9
Dawnixt <32944727+Dawnixt@users.noreply.github.com>
2018-03-20 08:46:23 +0100

    Hola

---
+++ Barco.java
@@ -0,0 +1,188 @@
+/*
+ * Nombre de la clase: Barco
+ * 
+ * ******Propiedades basicas*******
+ * 
+ * -Nombre->String Consultable y Modificable
+ * -Size->Int Consultable 
+ * -ID->Int Consultable
+ * 
+ * ******Propiedades derivadas******
+ * 
+ * -No hay 
+ * 
+ * ******Propiedades compartidas*******
+ * 
+ * -ContadorID-> int Consultable 
+ * 
+ * ********Restriciones*********
+ * 
+ * -size >= 1 y size < 6
+ * 
+ **************Interfaz**************
+ * 
+ * -public String getNombre()
+ * 
+ * -public void setNombre(String nombre)
+ * 
+ * -public int getSize()
+ * 
+ * -public int getConatdorID()
+ * 
+ * -public int getID()
+ * 
+ * *******Funcionalidades añadidas********
+ * 
+ * -No hay 
+ * 
+ */
+
+
+package dawnixt.hundirlaflota.clases;
+
+public class Barco implements Comparable<Barco>, Cloneable {
+	
+	private String nombre;
+	private int size;
+	private int ID;
+	private static int contadorID=0;
+	
+	
+	//Constructores
+	
+	//Por defecto
+	public Barco(){
+		
+		contadorID++;
+		ID=contadorID;
+		nombre="Sin nombre";
+		size=1;
+	}
+	
+	//Con parametros
+	public Barco(String nombre, int size) {
+		
+		contadorID++;
+		ID=contadorID;
+		this.nombre=nombre;
+		this.size=size;
+	}
+	
+	//De copia
+	public Barco(Barco barco) {
+		
+		this.ID=barco.ID;
+		this.size=barco.size;
+		this.nombre=barco.nombre;
+	}
+	
+	//Gets y Sets
+	
+	public String getNombre() {
+		
+		return(nombre);
+	}
+	
+	public void setNombre(String nombre) {
+		
+		this.nombre=nombre;
+	}
+	
+	public int getSize() {
+		
+		return(size);
+	}
+	
+	public int getID() {
+		
+		return(ID);
+	}
+	
+	public int getConatdorID() {
+		
+		return(contadorID);
+	}
+	
+	//Metodos sobreescritos
+	
+	@Override
+	public int hashCode(){
+		
+		return(this.size*this.ID);
+	}
+	
+	//Criterios de igualacion dos barcos son iguales cuando su size y su nombre
+	@Override 
+	public boolean equals(Object obj) {
+		
+		boolean ret=false;
+		
+		if(this==obj) {
+			
+			ret=true;
+		}
+		
+		else if(obj!=null && obj instanceof Barco) {
+			
+			Barco other=(Barco) obj;
+			
+			if(this.size==other.size &&
+				this.nombre==other.nombre) 
+				
+				ret=true;
+			
+		}
+		return(ret);
+	}
+	
+	@Override
+	public Barco clone() {
+		
+		Barco copia=null;
+		
+		try {
+			
+			copia=(Barco) super.clone();
+		}
+		catch(CloneNotSupportedException error) {
+			
+			System.out.println("No se ha podido realizar el clone");
+		}
+		return(copia);
+	}
+	
+	/*Condiciones de comparacion devolvera:
+	*	-1 si el size es mayor que el del otro barco
+	*	-0 si el size es igual al del otro barco
+	*	- -1 si el size es menor que el del otro barco
+	*/
+	@Override
+	public int compareTo(Barco other){
+		
+		int devolucion=0;
+		
+		if(this!=other && this.getSize()> other.getSize()) {
+			
+			devolucion=1;
+		}
+		
+		else if (this!=other && this.getSize()<other.getSize()) {
+			
+			devolucion=-1;
+		}
+		
+		else {
+			
+			devolucion=0;
+		}
+		
+		return(devolucion);
+	}
+	
+	@Override
+	public String toString() {
+		
+		return("Su barco tiene: como nombre"+this.getNombre()+"y tiene"+this.getSize()+"piezas y tiene el siguiente ID: "+ this.getID());
+	}
+
+}
---
+++ TestBarco.java
@@ -0,0 +1,47 @@
+package dawnixt.hundirlaflota.clases;
+
+public class TestBarco {
+	
+	public static void main (String[] args) {
+		
+		//Constructores
+		Barco barcopordefecto=new Barco();
+		Barco barcoparametros=new Barco("SS.D.VA",3);
+		Barco barcodecopia=new Barco(barcoparametros);
+		Barco clonebarcoparametros=null;
+		Barco clonebarcocopia=null;
+		
+		//Gets y Sets
+		System.out.println("Valores del barco 1"+ barcopordefecto.toString());
+		System.out.println("Valores del barco 2"+ barcoparametros.toString());
+		System.out.println("Los valores del barco 2 usando el barco 3"+ barcodecopia.toString());
+		barcoparametros.setNombre("SS.Oscar");
+		System.out.println("Valores despues del set del valor del barco 2"+barcoparametros.getNombre());
+		barcopordefecto.setNombre("Barco generico numero 1");
+		
+		//Metodos sobrecargados y sobreescritos
+		
+		//Clone
+		clonebarcoparametros=barcoparametros.clone();
+		System.out.println("Clone de barco con parametros"+clonebarcoparametros.toString());
+		clonebarcocopia=barcodecopia.clone();
+		System.out.println("Clone del barco de copia"+clonebarcocopia.toString());
+		
+		//hashCode
+		System.out.println("El hahCode de todos los barcos en orden:");
+		System.out.println("Barco 1"+barcopordefecto.hashCode());
+		System.out.println("Barco 2"+barcoparametros.hashCode());
+		System.out.println("Barco 3"+barcodecopia.hashCode());
+		System.out.println("Barco clone del barco 2"+clonebarcoparametros.hashCode());
+		System.out.println("Barco clone del barco de copia"+clonebarcocopia.hashCode());
+		
+		//equals
+		System.out.println("Probando el equals en el barco 1 y 2:"+barcopordefecto.equals(barcoparametros));
+		System.out.println("Equals en el barco 1 y en el clone de 2"+barcopordefecto.equals(clonebarcoparametros));
+		
+		//clone
+		System.out.println("");
+		
+	}
+	
+}
